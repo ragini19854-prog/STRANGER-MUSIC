10c37defbe0527cc9b6d17381d1824c75e36b73f
ragini19854-prog <ragini.19854@gmail.com>
2026-05-13 09:28:37 +0530

    Update welcome.py

--- SHUKLAMUSIC/plugins/tools/welcome.py
+++ SHUKLAMUSIC/plugins/tools/welcome.py
@@ -78,7 +78,19 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 
-def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):background = Image.open("SHUKLAMUSIC/assets/welcome2.png")pfp = Image.open(pic).convert("RGBA")pfp = circle(pfp, brightness_factor=brightness_factor)pfp = pfp.resize((500, 500))draw = ImageDraw.Draw(background)font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=60)
+def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
+    background = Image.open("SHUKLAMUSIC/assets/welcome2.png")
+    pfp = Image.open(pic).convert("RGBA")
+    pfp = circle(
+        pfp,
+        brightness_factor=brightness_factor
+    )
+    pfp = pfp.resize((500, 500))
+    draw = ImageDraw.Draw(background)
+    font = ImageFont.truetype(
+        "SHUKLAMUSIC/assets/font.ttf",
+        size=60
+    )
 
 # Updated ID position to (630, 450)
 draw.text((630, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
