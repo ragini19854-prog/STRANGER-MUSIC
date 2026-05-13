77803d1c433de281bc41aee97e21fd35d7a921bd
ragini19854-prog <ragini.19854@gmail.com>
2026-05-13 07:29:48 +0530

    Update welcome.py

--- SHUKLAMUSIC/plugins/tools/welcome.py
+++ SHUKLAMUSIC/plugins/tools/welcome.py
@@ -82,7 +82,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     background = Image.open("SHUKLAMUSIC/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor)
-    pfp = pfp.resize((500, 500))
+    pfp = pfp.resize((322, 322))
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=60)
     
@@ -90,7 +90,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     draw.text((630, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
     
     # Updated PFP position to (48, 88)
-    pfp_position = (48, 88)
+    pfp_position = (62, 87)
     background.paste(pfp, pfp_position, pfp)
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
